86abe188f3c234a86783b4b3fdc447700c2822c1
Tin Phan <phanthanhtincr7@gmail.com>
2025-07-04 18:01:20 +0700

    fix: fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import ConfigSetting from "./components/ConfigSetting";
-import EditDetails from "./components/EditDetails";
+import {SimpleEditor} from "./components/tiptap-templates/simple/simple-editor";
 
-const steps = ["Select Subject & Configuration Settings", "Edit details"];
 const App = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const handleNextStep = async () => {
-    setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
-  };
-  const handlePrevStep = () => {
-    if (currentStep > 0) {
-      setCurrentStep((prev) => prev - 1);
-    }
-  };
-  return (
-    <div className="">
-      {currentStep === 0 ? (
-        <ConfigSetting onNextStep={handleNextStep} />
-      ) : (
-        <EditDetails onPrevStep={handlePrevStep} />
-      )}
-    </div>
-  );
+	return <SimpleEditor />;
 };
 
 export default App;
